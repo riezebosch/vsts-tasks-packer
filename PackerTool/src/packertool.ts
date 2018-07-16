c0468cee3875e0677611bf9064df6ecee55fb364
Manuel Riezebosch <mriezebosch@gmail.com>
2018-07-16 13:23:28 +0200

    extract zip on al platforms

--- PackerTool/src/packertool.ts
+++ PackerTool/src/packertool.ts
@@ -1,11 +1,6 @@
 import * as taskLib from 'vsts-task-lib/task';
 import * as toolLib from 'vsts-task-tool-lib/tool';
-import * as restm from 'typed-rest-client/RestClient';
 import * as os from 'os';
-import * as path from 'path';
-
-let osPlat: string = os.platform();
-let osArch: string = os.arch();
 
 async function run() {
     try {
@@ -17,30 +12,6 @@ async function run() {
     }
 }
 
-//
-// Node versions interface
-// see https://nodejs.org/dist/index.json
-//
-interface INodeVersion {
-    version: string,
-    files: string[]
-}
-
-//
-// Basic pattern:
-//      if !checkLatest
-//          toolPath = check cache
-//      if !toolPath
-//          if version is a range
-//              match = query nodejs.org
-//              if !match
-//                  fail
-//              toolPath = check cache
-//          if !toolPath
-//              download, extract, and cache
-//              toolPath = cacheDir
-//      PATH = cacheDir + PATH
-//
 async function getNode(version: string) {
     // check cache
     let toolPath: string = toolLib.findLocalTool('packer', version);
@@ -67,20 +38,7 @@ async function acquireNode(version: string): Promise<string> {
     //
     // Extract
     //
-    let extPath: string;
-    if (osPlat == 'win32') {
-        taskLib.assertAgent('2.115.0');
-        extPath = taskLib.getVariable('Agent.TempDirectory');
-        if (!extPath) {
-            throw new Error('Expected Agent.TempDirectory to be set');
-        }
-
-        let _7zPath = path.join(__dirname, '7za.exe');
-        extPath = await toolLib.extract7z(downloadPath, extPath, _7zPath);
-    }
-    else {
-        extPath = await toolLib.extractTar(downloadPath);
-    }
+    let extPath: string = await toolLib.extractZip(downloadPath);
 
     //
     // Install into the local tool cache
